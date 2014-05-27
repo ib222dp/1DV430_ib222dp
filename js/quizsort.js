ec38b9c7069df4a2c98ea8f2f6ec2aa96f22e86a
ib222dp <ib222dp@student.lnu.se>
2014-05-27 07:24:02 +0200

    Javascript

--- js/quizsort.js
+++ js/quizsort.js
@@ -10,25 +10,25 @@ var Sorter = {
     init: function () {
 
         //Hämtar klass för div "content" för att avgöra vilken XML-fil som ska läsas in
-        var contentClass = document.getElementById("content").getAttribute("class");
+        var contentClass = document.getElementById("contentheight").getAttribute("class");
         var url = "../XML/" + contentClass + ".xml";
 
-        //Öppnar XML-filen (http://stackoverflow.com/questions/22495746/using-javascript-to-show-xml-info-in-html-webpage?rq=1)
-        var xhr = new XMLHttpRequest();
-        xhr.open("get", url, false);
-        xhr.send(null);
-        var xmlDoc = xhr.responseXML;
+        //Skapande av en ny AjaxCon-instans
+        new AjaxCon(url, function (data) {
 
-        //Läser in quizzen till egenskapen quizArray
-        var i, quizzes = xmlDoc.getElementsByTagName("quizzes")[0];
+            var xmlDoc = data;
 
-        for (i = 0; i < quizzes.children.length; i += 1) {
-            var quiz = xmlDoc.getElementsByTagName("quiz")[i];
-            Sorter.quizArray.push(quiz);
-            Sorter.quizArray[i].date = new Date(quiz.getAttribute("created"));
-            Sorter.quizArray[i].name = quiz.getAttribute("name");
-        }
-        Sorter.showquizzes();
+            //Läser in quizzen till egenskapen quizArray
+            var i, quizzes = xmlDoc.getElementsByTagName("quizzes")[0];
+
+            for (i = 0; i < quizzes.children.length; i += 1) {
+                var quiz = xmlDoc.getElementsByTagName("quiz")[i];
+                Sorter.quizArray.push(quiz);
+                Sorter.quizArray[i].date = new Date(quiz.getAttribute("created"));
+                Sorter.quizArray[i].name = quiz.getAttribute("name");
+            }
+            Sorter.showquizzes();
+        });
     },
 
     showquizzes: function () {
@@ -52,7 +52,7 @@ var Sorter = {
             dl.appendChild(dtTag);
         }
         quizzesDiv.appendChild(dl);
-        document.getElementById("content").appendChild(quizzesDiv);
+        document.getElementById("contentheight").appendChild(quizzesDiv);
 
         //Händelsehanterare kopplad till "Bokstavsordning" och "Nyaste först"-knapparna
         for (k = 0; k < Sorter.sortButtons.length; k += 1) {
